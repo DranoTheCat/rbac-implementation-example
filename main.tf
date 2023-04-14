673eed64a4fd48c6a917a68796fee88bc90431d7
DranoTheCat <47464311+DranoTheCat@users.noreply.github.com>
2023-04-13 20:52:30 -0700

    Add encrypted access keys

--- main.tf
+++ main.tf
@@ -3,10 +3,12 @@ locals {
     {
       username    = "dhart"
       fullname    = "Dan Hart"
+      gpg_pub_key = "public_gpg_keys/dhart.gpg"
     },
     {
       username    = "userone"
       fullname    = "User One"
+      gpg_pub_key = "public_gpg_keys/userone.gpg"
     }
   ]
 }
--- sitereliability_engineers.tf
+++ sitereliability_engineers.tf
@@ -13,6 +13,15 @@ resource "aws_iam_group" "sitereliability_engineering" {
   path = "/"
 }
 
+resource "aws_iam_access_key" "sitereliability_engineers" {
+  for_each = {
+    for index, engineer in local.sitereliability_engineers:
+    engineer.username => engineer
+  }
+  user = each.value.username
+  pgp_key = data.local_file.sitereliability_engineer_gpg_public_keys[each.key].content
+}
+
 resource "aws_iam_group_membership" "sitereliability_engineers" {
   name = "sitereliability_engineers"
   users = [for user in aws_iam_user.sitereliability_engineers : user.name]
